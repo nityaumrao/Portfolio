d0d68f8018950e7de7ed834ecffa99c5e76a243e
nityaumrao <nityaumrao@gmail.com>
2025-05-12 14:50:33 +0530

    Innitial Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Skills from './components/Skills';
+import Works from './components/Work';
+
 
 function App() {
   return (
     <div className="App">
+
+      <About />
+      <Home />
+      <Navbar />
+      <Contact />
+      <Skills />
+      <Works />
       
     </div>
   );
